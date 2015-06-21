334e5283b70c4542fdadb6836f8927cf894098b8
Nikhil Agrawal <nikhil.pa@gmail.com>
2015-06-21 15:46:44 +0530

    adding kryo example

---
+++ src/com/freemindcafe/serialization/kryo/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * <p>
+ * XMLAliasing provides a way to map classes to xml elements.
+ * </p>
+ * <p>
+ * Xstream is the main API.
+ * </p>
+ * <p>
+ * Jettison is used to get an JSON structure.
+ * </p>
+ * <p>
+ * Jackson is arguably the standard Java Library for processing JSON.
+ * </p>
+ * <ul>
+ * <li>com.thoughtworks.xstream</li>
+ * <li>org.codehaus.jackson</li>
+ * <li>org.codehaus.jettison</li>
+ * </ul>
+ */
+package com.freemindcafe.serialization.kryo;
