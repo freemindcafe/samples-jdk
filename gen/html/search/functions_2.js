69393c2b8ba728804116fa3ad1b4a2dbb295657f
Nikhil Agrawal <nikhil.pa@gmail.com>
2015-06-24 21:30:20 +0530

    modifying

--- gen/html/search/functions_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['jaxwsproxyfactorybean_5fbased_5fclient',['JaxWsProxyFactoryBean_based_client',['../classcom_1_1freemindcafe_1_1apache_1_1cxf_1_1wsdl_1_1sample1_1_1_client.html#aa91f411f3bb32b7e9f34d3ebaa6a6618',1,'com::freemindcafe::apache::cxf::wsdl::sample1::Client']]],
-  ['jsontolistofobject',['jsonToListOfObject',['../classcom_1_1freemindcafe_1_1serialization_1_1sample3_1_1_j_s_o_n_parser_util.html#afb889b078691cec90cc0ec178e668010',1,'com::freemindcafe::serialization::sample3::JSONParserUtil']]],
-  ['jsontoobject',['jsonToObject',['../classcom_1_1freemindcafe_1_1serialization_1_1sample3_1_1_j_s_o_n_parser_util.html#a6510d634e162ade47c8d41843603b633',1,'com::freemindcafe::serialization::sample3::JSONParserUtil']]]
-];
