6582bffdab4748e7f04ee0604587847b2161899d
Nikhil Agrawal <nikhil.pa@gmail.com>
2015-06-27 22:14:28 +0530

    Removing the old documentation

--- gen/html/search/classes_e.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['test',['Test',['../classcom_1_1freemindcafe_1_1apache_1_1velocity_1_1sample1_1_1_test.html',1,'com::freemindcafe::apache::velocity::sample1']]],
-  ['test',['Test',['../classcom_1_1freemindcafe_1_1apache_1_1commons_1_1beanutils_1_1sample2_1_1_test.html',1,'com::freemindcafe::apache::commons::beanutils::sample2']]],
-  ['test',['Test',['../classcom_1_1freemindcafe_1_1apache_1_1commons_1_1beanutils_1_1sample1_1_1_test.html',1,'com::freemindcafe::apache::commons::beanutils::sample1']]]
-];
