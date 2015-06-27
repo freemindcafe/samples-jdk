7d2a2a9855dd296e19a7d5cc3bce3497c3b580b9
Nikhil Agrawal <nikhil.pa@gmail.com>
2015-06-27 22:09:06 +0530

    Removing the old documentation

--- gen/html/search/classes_9.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['mapwrapper',['MapWrapper',['../classcom_1_1freemindcafe_1_1serialization_1_1sample2_1_1_map_wrapper.html',1,'com::freemindcafe::serialization::sample2']]],
-  ['messageprocessor',['MessageProcessor',['../classcom_1_1freemindcafe_1_1oops_1_1innerclass_1_1library_1_1_message_processor.html',1,'com::freemindcafe::oops::innerclass::library']]],
-  ['myerrorhandler',['MyErrorHandler',['../classcom_1_1freemindcafe_1_1xml_1_1xsd_1_1_my_error_handler.html',1,'com::freemindcafe::xml::xsd']]],
-  ['mystringtype',['MyStringType',['../interfaceorg_1_1apache_1_1hello_world_soap_http_1_1types_1_1_my_string_type.html',1,'org::apache::helloWorldSoapHttp::types']]],
-  ['mystringtypeimpl',['MyStringTypeImpl',['../classorg_1_1apache_1_1hello_world_soap_http_1_1types_1_1impl_1_1_my_string_type_impl.html',1,'org::apache::helloWorldSoapHttp::types::impl']]]
-];
