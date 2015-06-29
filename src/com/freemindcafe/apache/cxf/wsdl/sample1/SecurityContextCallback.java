88d1a44191674354db7aa08e8170a471b5527676
Nikhil Agrawal <nikhil.pa@gmail.com>
2015-06-29 21:22:16 +0530

    Reverting back to cxf 2.x sample. Sample3 has uncompiled code for cxf 3.0.3

--- src/com/freemindcafe/apache/cxf/wsdl/sample1/SecurityContextCallback.java
+++ src/com/freemindcafe/apache/cxf/wsdl/sample1/SecurityContextCallback.java
@@ -7,8 +7,7 @@ import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
 import org.apache.log4j.Logger;
-import org.apache.wss4j.common.ext.WSPasswordCallback;
-
+import org.apache.ws.security.WSPasswordCallback;
 
 //\cond HIDDEN_SYMBOLS
 /**
