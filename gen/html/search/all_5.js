845d861b6af44e5457e74cdeeba52701d76ad94a
Nikhil Agrawal <nikhil.pa@gmail.com>
2015-11-29 19:11:27 +0530

    removing the old version

--- gen/html/search/all_5.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['faultdetail',['FaultDetail',['../interfaceorg_1_1apache_1_1hello_world_soap_http_1_1types_1_1_fault_detail_document_1_1_fault_detail.html',1,'org::apache::helloWorldSoapHttp::types::FaultDetailDocument']]],
-  ['faultdetaildocument',['FaultDetailDocument',['../interfaceorg_1_1apache_1_1hello_world_soap_http_1_1types_1_1_fault_detail_document.html',1,'org::apache::helloWorldSoapHttp::types']]],
-  ['faultdetaildocumentimpl',['FaultDetailDocumentImpl',['../classorg_1_1apache_1_1hello_world_soap_http_1_1types_1_1impl_1_1_fault_detail_document_impl.html',1,'org::apache::helloWorldSoapHttp::types::impl']]],
-  ['firsttest',['FirstTest',['../classcom_1_1freemindcafe_1_1jmx_1_1sample3_1_1_first_test.html',1,'com::freemindcafe::jmx::sample3']]],
-  ['foobean',['FooBean',['../classcom_1_1freemindcafe_1_1apache_1_1log4j_1_1sample1_1_1_foo_bean.html',1,'com::freemindcafe::apache::log4j::sample1']]],
-  ['foobean',['FooBean',['../classcom_1_1freemindcafe_1_1apache_1_1log4j_1_1sample2_1_1_foo_bean.html',1,'com::freemindcafe::apache::log4j::sample2']]]
-];
--- gen/html/search/all_7.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['helloservice',['HelloService',['../classcom_1_1freemindcafe_1_1jmx_1_1sample1_1_1server_1_1_hello_service.html',1,'com::freemindcafe::jmx::sample1::server']]],
-  ['helloservice',['HelloService',['../classcom_1_1freemindcafe_1_1jmx_1_1sample2_1_1server_1_1_hello_service.html',1,'com::freemindcafe::jmx::sample2::server']]],
-  ['helloserviceimpl',['HelloServiceImpl',['../classcom_1_1freemindcafe_1_1jmx_1_1sample2_1_1client_1_1_hello_service_impl.html',1,'com::freemindcafe::jmx::sample2::client']]],
-  ['helloservicembean',['HelloServiceMBean',['../interfacecom_1_1freemindcafe_1_1jmx_1_1sample2_1_1server_1_1_hello_service_m_bean.html',1,'com::freemindcafe::jmx::sample2::server']]],
-  ['helloservicembean',['HelloServiceMBean',['../interfacecom_1_1freemindcafe_1_1jmx_1_1sample1_1_1server_1_1_hello_service_m_bean.html',1,'com::freemindcafe::jmx::sample1::server']]],
-  ['httpsclienttest',['HttpsClientTest',['../classcom_1_1freemindcafe_1_1apache_1_1commons_1_1_https_client_test.html',1,'com::freemindcafe::apache::commons']]]
-];
