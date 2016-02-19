05171a10bd8fdff1688d686fecdc6354fab6d576
Nikhil Agrawal <nikhil.pa@gmail.com>
2016-02-19 08:45:30 +0530

    Adding Doxygen cheat sheet

--- src/com/freemindcafe/apache/cxf/package-info.java
+++ src/com/freemindcafe/apache/cxf/package-info.java
@@ -1,9 +1,17 @@
 /**
  * \brief Use soapUI to send the soap requests and jaxrs requests involving the SSL.
  * 
- * * \image html SoapUI 5.0.0_1.png
- * * \image html SoapUI 5.0.0_2.png
- * * \image html SoapUI 5.0.0_3.png
+ * <p>
+ * \image html SoapUI_5.0.0_1.png
+ * </p>
+ * <br>
+ * <p>
+ * \image html SoapUI_5.0.0_2.png
+ * </p>
+ * <br>
+ * <p>
+ * \image html SoapUI_5.0.0_3.png
+ * </p>
  * 
  */
 package com.freemindcafe.apache.cxf;
